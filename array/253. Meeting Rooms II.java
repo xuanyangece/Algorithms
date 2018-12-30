42843bae7546e6224a8619ad72dae1f8e5a91aec
Justin Yang <37907441+xuanyangece@users.noreply.github.com>
2018-12-29 23:31:09 -0500

    Create 253. Meeting Rooms II.java

---
+++ array/253. Meeting Rooms II.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+class Solution {
+    public int minMeetingRooms(Interval[] intervals) {
+        if (intervals == null || intervals.length == 0) return 0;
+        
+        int[] starts = new int[intervals.length];
+        int[] ends = new int[intervals.length];
+        for (int i = 0; i < intervals.length; i++) {
+            starts[i] = intervals[i].start;
+            ends[i] = intervals[i].end;
+        }
+        
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+        
+        int ans = 0, nextMinEnd = 0;
+        for (int i = 0; i < starts.length; i++) {
+            if (starts[i] < ends[nextMinEnd]) ans++;
+            else nextMinEnd++;
+        }
+        
+        return ans;
+    }
+}
