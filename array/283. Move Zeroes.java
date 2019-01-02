73915ea07a3144140da9396ddb1e880744550474
Justin Yang <37907441+xuanyangece@users.noreply.github.com>
2019-01-02 16:45:40 -0500

    Create 283. Move Zeroes.java

---
+++ array/283. Move Zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length <= 1) return;
+        
+        int insert = 0;
+        for (int num: nums) {
+            if (num != 0) nums[insert++] = num;
+        }
+        
+        while (insert < nums.length) {
+            nums[insert++] = 0;
+        }
+    }
+}
