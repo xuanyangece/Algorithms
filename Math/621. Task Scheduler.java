28391ddb9e0ae5a47d6558b545fde0d338e92cb0
Justin Yang <37907441+xuanyangece@users.noreply.github.com>
2019-01-01 21:06:24 -0500

    Create 621. Task Scheduler.java

---
+++ Math/621. Task Scheduler.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] count = new int[26];
+        for (char c: tasks) {
+            count[c - 'A']++;
+        }
+        Arrays.sort(count);
+        
+        int i = 25;
+        while (i >=0 && count[i] == count[25]) i--;
+        
+        return Math.max(tasks.length, (n + 1) * (count[25] - 1) + 25 - i);
+    }
+}
