4a8dc0ce05853e6e730bc26814f7246ce6864210
Justin Yang <37907441+xuanyangece@users.noreply.github.com>
2019-01-03 22:17:34 -0500

    Create 67. Add Binary.java

---
+++ Math/67. Add Binary.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        int carry = 0;
+        int i = a.length() - 1, j = b.length() - 1;
+        StringBuilder sb = new StringBuilder();
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            if (j >= 0) sum += b.charAt(j--) - '0';
+            sb.append(sum % 2);
+            carry = sum >> 1;
+        }
+        if (carry > 0) sb.append('1');
+        
+        return sb.reverse().toString();
+    }
+}
