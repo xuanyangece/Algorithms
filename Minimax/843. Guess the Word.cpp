3ad859366df9f27ee02ee675ca9957775789159f
Justin Yang <37907441+xuanyangece@users.noreply.github.com>
2019-05-22 18:13:15 +0800

    Create 843. Guess the Word.cpp

---
+++ Minimax/843. Guess the Word.cpp
@@ -0,0 +1,39 @@
+/**
+ * // This is the Master's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class Master {
+ *   public:
+ *     int guess(string word);
+ * };
+ */
+class Solution {
+public:
+    void findSecretWord(vector<string>& wordlist, Master& master) {
+        for (int i = 0, x = 0; i < 10 && x < 6; ++i) {
+            unordered_map<string, int> count;
+            for (string w1 : wordlist)
+                for (string w2 : wordlist)
+                    if (match(w1, w2) == 0)
+                        count[w1]++;
+            pair<string, int> minimax = make_pair(wordlist[0], 1000);
+            for (string w : wordlist)
+                if (count[w] <= minimax.second)
+                    minimax = make_pair(w, count[w]);
+            x = master.guess(minimax.first);
+            vector<string> wordlist2;
+            for (string w : wordlist)
+                if (match(minimax.first, w) == x)
+                    wordlist2.push_back(w);
+            wordlist = wordlist2;
+        }
+    }
+    
+    int match(string s1, string s2) {
+        int matches = 0;
+        for (int i = 0; i < s1.size(); i++) {
+            if (s1[i] == s2[i]) matches++;
+        }
+        
+        return matches;
+    }
+};
