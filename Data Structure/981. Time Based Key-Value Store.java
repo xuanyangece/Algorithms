42c3c87a6bbb99946abb84793d20c8233299b62b
Justin Yang <37907441+xuanyangece@users.noreply.github.com>
2019-05-23 18:19:22 +0800

    Create 981. Time Based Key-Value Store.java

---
+++ Data Structure/981. Time Based Key-Value Store.java
@@ -0,0 +1,31 @@
+class TimeMap {
+    Map<String, TreeMap<Integer, String>> map;
+    /** Initialize your data structure here. */
+    public TimeMap() {
+        map = new HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        if (!map.containsKey(key)) {
+            map.put(key, new TreeMap<>());
+        }
+        map.get(key).put(timestamp, value);
+    }
+    
+    public String get(String key, int timestamp) {
+        if (!map.containsKey(key)) return "";
+        
+        TreeMap<Integer, String> values = map.get(key);
+        Integer floor = values.floorKey(timestamp);
+        if (floor == null) return "";
+        
+        return values.get(floor);
+    }
+}
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
